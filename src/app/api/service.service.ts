ec0ac5ce41af792307ea1d7b1aae28aa2909c55e
Hideki3 <hideki3@hotmail.com>
2022-05-23 22:25:33 -0300

    Atualizado

--- src/app/api/service.service.ts
+++ src/app/api/service.service.ts
@@ -2,39 +2,55 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Md5} from 'ts-md5/dist/md5';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-    private publicKey ="";
-    private privateKey = "";
+  private publicKey  = "";
+  private privateKey = "";
 
-    private host = "http://gateway.marvel.com/";
+  private host = "http://gateway.marvel.com/";
 
-    constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){}
+
+  public getDados(url: string, parameters: string){
+    let ts = this.generateTs();
+    
+    return new Promise((ret) => {
+      this.http.get(this.host + url + '?ts=' 
+      + ts + '&apikey=' + this.publicKey 
+      + '&hash=' + this.getHash(ts) + parameters).subscribe((response) => {
+        
+        if(response){
+          ret(response);
+
+        } else {
+          ret(false);
+        }
+      })          
+    })
+
+  }
   
-    public getDados(url: String,parameters:String){
-      
+  private generateTs(){
+    return Math.floor(100000 + Math.random() * 900000);
+  }
 
-      let ts = "generateTs";
+  private getHash(ts){
+    return Md5.hashStr(ts + this.privateKey + this.publicKey);
+  }
 
-      return new Promise((ret) => {
-        this.http.get(this.host + url + '?ts=' + ts + '&apikey' + this.publicKey + '&hash=' + 0 + parameters).subscribe((response) => {
+  private getKeys(){
+    return new Promise((ret) => {
+      this.http.get('assets/keys.json').subscribe((keys:any) => {
+        this.publicKey  = keys.public;
+        this.privateKey = keys.private;
 
-          if(response){
-            ret(response);
-          }else {
-            ret(false);
-          }
-
-        })
-
+        ret(true);
       })
-    }
+    })
+  }
 
-    private generateTs(){
-      return Math.floor(100000000 + Math.random() * 90000000000);
-    }
 
 }
+
--- src/app/provider/character.service.ts
+++ src/app/provider/character.service.ts
@@ -7,18 +7,55 @@ import { PaginationComponent } from './../util/pagination/pagination.component';
 })
 export class CharacterService {
 
-  constructor(private service: ServiceService) { }
+  constructor(private service: ServiceService) {}
 
-  public getCharacterById(id:number){
-    return new Promise((ret) =>{
-      this.service.getDados('/v1/public/characters/'+ id, '')
-      .then((data: any)=>{
-       if(data && data.data && data.data.results){
-         ret(data.data.results);
-       }else{
-         ret([]);
-       }
+  public getCharacterById(id: number){
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters/' + id, '')
+      .then((data: any) => {
+          if(data && data.data && data.data.results){
+            ret(data.data.results);
 
+          } else {
+
+            ret([]);
+          }
+
+      })
+    })
+  }
+
+  public getAllCharacters(pagination: PaginationComponent, filter: string){
+    let strFilter = '';
+
+    let param = '&limit=' + pagination.getLimit() + '&offset=' + pagination.getOffset() + strFilter;
+
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters', 
+      param).then((data:any) => {
+        if(data && data.data && data.data.results){
+          this.updatePagination(pagination, data.data);
+          ret(data.data.results);
+
+        } else {
+          ret([]);
+
+        }
+      })
+    })
+
+  }
+
+  public getComicsByCharacter(id: number){
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters/' + id + '/comics', '').then((data:any) => {
+        if(data && data.data && data.data.results){
+          ret(data.data.results);
+
+        } else {
+          ret([]);
+
+        }
       })
     })
   }
@@ -30,3 +67,4 @@ export class CharacterService {
     pagination.createPages();
   }
 }
+
