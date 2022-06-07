3a30e76edf090278df41e3ce981170341b85871c
Hideki3 <Hideki3@users.noreply.github.com>
2022-06-07 13:25:52 -0300

    Atualizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,71 +10,73 @@ import { PaginationComponent } from '../util/pagination/pagination.component';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  public characters : any=[];
+  
+  /* objeto utilizado para controlar o filtro */
+  public filtro   = { 
+      descricao   : '',
+      bkp         : ''
+  };
+    
+  public pagination = new PaginationComponent();
+  public checking   = true;
 
-    public filtro = {
-      descricao: '',
-      bkp      : ''
-    }
+  constructor(private characterService: CharacterService, private navCtrl: NavController) {
+    this.pagination.setLimit(10);
+    this.getAllCharacters();
 
-    public characters = [];
-    public pagination = new PaginationComponent();
-    public checking = false;
+  }
 
-    constructor(private characterService: CharacterService,
-                private navCtrl: NavController){
-      this.pagination.setLimit(10);
-      this.getAllCharacters();
-    }
-
-
-    public getAllCharacters(){
+  /* recupera personagens de acordo com a paginação */
+  public getAllCharacters(){
       this.checking = true;
-
+      
+      /* verifica filtro e reseta paginação */
       if(this.filtro.descricao != this.filtro.bkp){
-        this.pagination.reset();
+          this.pagination.reset();
       }
-
-      this.characterService.getAllCharacters(this.pagination , "")
-      .then((characters:any) => {
-        console.log(characters);
-        this.filtro.bkp = this.filtro.descricao;
-        this.characters = [];
-        this.characters = characters;
-        this.checking = false;
+      
+      this.characterService.getAllCharacters(this.pagination, this.filtro.descricao).then((characters:any) => {
+          this.filtro.bkp = this.filtro.descricao;
+          this.characters = [];
+          this.characters = characters;
+          this.checking = false;
       });
+  }
 
-    }
+  /* métodos relacionados à paginação */
+  public goFirstPage(){
+      this.pagination.setCurrentPage(1);
+      this.getAllCharacters();
+  }
 
-    public goDetails(character: any) {
-      this.navCtrl.navigateForward('character', {
-        queryParams: { id : character.id }
-      });
-    }
+  public goLastPage(){
+      this.pagination.setCurrentPage(this.pagination.getPages()[this.pagination.getPages().length - 1]);
+      this.getAllCharacters();
+  }
 
-     /* métodos relacionados à paginação */
-      public goFirstPage(){
-        this.pagination.setCurrentPage(1);
-        this.getAllCharacters();
-      }
+  public goPreviousPage(){
+      this.pagination.setCurrentPage(this.pagination.getCurrentPage() - 1);
+      this.getAllCharacters();
+  }
 
-      public goLastPage(){
-          this.pagination.setCurrentPage(this.pagination.getPages()[this.pagination.getPages().length - 1]);
-          this.getAllCharacters();
-      }
+  public goNextPage(){
+      this.pagination.setCurrentPage(this.pagination.getCurrentPage() + 1);
+      this.getAllCharacters();
+  }
 
-      public goPreviousPage(){
-          this.pagination.setCurrentPage(this.pagination.getCurrentPage() - 1);
-          this.getAllCharacters();
-      }
+  public goPage(page: number){
+      this.pagination.setCurrentPage(page);
+      this.getAllCharacters();
+  }
+  /* --- */
 
-      public goNextPage(){
-          this.pagination.setCurrentPage(this.pagination.getCurrentPage() + 1);
-          this.getAllCharacters();
-      }
+  /* vai para os detalhes do personagem */
+  public goDetails(character: any){
+    this.navCtrl.navigateForward('character', {
+        queryParams: { id: character.id }
+    });
+  }   
 
-      public goPage(page: number){
-          this.pagination.setCurrentPage(page);
-          this.getAllCharacters();
-      }
-      /* --- */
 }
